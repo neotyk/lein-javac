152b340c3206c2bedc37d61b158b43165b34fbc5
jared strate <strate.jared@gmail.com>
2010-02-02 16:21:56 -0800

    initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject leiningen/lein-javac "1.0.0"
+  :description "Java compile plugin for leiningen"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [org.apache.maven/maven-ant-tasks "2.0.10"]])
---
+++ src/leiningen/javac.clj
@@ -0,0 +1,23 @@
+(ns leiningen.javac
+  (:use [leiningen.compile :only [make-path find-lib-jars]]
+        [clojure.contrib.java-utils :only [file]])
+  (:import org.apache.tools.ant.taskdefs.Javac)
+  (:import org.apache.tools.ant.types.Path)
+  (:require lancet))
+
+(defn javac
+"creates and executes javac ant task.  Assumes $JAVA_HOME is set"
+  [project]
+  (let [java (Javac.)]
+    (doto java
+      (.setProject lancet/ant-project)
+      (.setSrcdir (make-path (:source-path project)))
+      (.setDestdir (file (:compile-path project)))
+      (.setClasspath (make-path (apply make-path
+                                       (find-lib-jars project))
+                                (:source-path project)
+                                (:test-path project)
+                                (:compile-path project)
+                                (:resources-path project)))
+      (.setFork true)
+      (.execute))))
